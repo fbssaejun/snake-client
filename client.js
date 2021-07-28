698d229c33bc18a42f0dd92d23a4704c8c447787
Anthony Kim <fbssaejun@gmail.com>
2021-07-28 17:48:50 +0000

    displays name of the snake, notifies player(me) that I am connected, notifies data from server to player

--- client.js
+++ client.js
@@ -3,18 +3,23 @@ const net = require('net')
 const connect = function () {
   const conn = net.createConnection({
     port: '50542',
-    host: '135.23.223.133'
-  });
+    host: '135.23.223.133'}
+    
+  );
+  conn.setEncoding('utf8');
 
-  const name = "Anthony"
+
   conn.on("connect", () => {
-    console.log(`${name} is connected!`)
+    console.log(`Successfully connected!`)
+    conn.write(`Name: ANK`)
   });
 
-  conn.on("data", (data) => {
-    console.log(data)
-  });
 
+
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
