3301f10134ffdaae0d8ff31840aa9d5d0300165e
Anthony Kim <fbssaejun@gmail.com>
2021-07-28 17:49:24 +0000

    handles user input (keyboard keys) from CLI and sends data to host server

--- play.js
+++ play.js
@@ -1,7 +1,25 @@
 const net = require("net");
+const stdin = process.stdin;
 const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
 
 
+const setupInput = function () {
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+
+  return stdin;
+};
+
+const handleUserInput =  (key) => {
+  if (key === '\u0003') {
+    console.log('Bye!')
+    process.exit();
+  }
+}; 
+
+setupInput()
